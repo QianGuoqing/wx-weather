cc1a3057c30fdba437469eeea4f7594311dce0eb
QianGuoqing <qian_guoqing@yeah.net>
2018-04-11 20:07:15 +0800

    获取网络天气api接口数据，数据动态显示，以及下拉刷新的功能

--- weather-app/pages/index/index.js
+++ weather-app/pages/index/index.js
@@ -1,3 +1,68 @@
+const weatherMap = {
+  sunny: '晴天',
+  cloudy: '多云',
+  overcast: '阴',
+  lightrain: '小雨',
+  heavyrain: '大雨',
+  snow: '雪'
+}
+
+const weatherColorMap = {
+  sunny: '#cbeefd',
+  cloudy: '#deeef6',
+  overcast: '#c6ced2',
+  lightrain: '#bdd5e1',
+  heavyrain: '#c5ccd0',
+  snow: '#aae1fc'
+}
+
 Page({
-  
+  data: {
+    nowTemperature: '',
+    nowWeather: '',
+    weatherBackground: ''
+  },
+  onLoad() {
+    this._getNowWeatherInformation()
+  },
+  onPullDownRefreash() {
+    this._getNowWeatherInformation(() => {
+      wx.stopPullDownRefresh()
+    })
+  },
+  _getNowWeatherInformation(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: 'chengdu'
+      },
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: (res) => {
+        // success
+        res = res.data
+        if (res.code === 200) {
+          let result = res.result
+          let { temp, weather } = result.now
+          this.setData({
+            nowTemperature: temp,
+            nowWeather: weatherMap[weather],
+            weatherBackground: `/images/${weather}-bg.png`
+          })
+
+          wx.setNavigationBarColor({
+            frontColor: '#000000',
+            backgroundColor: weatherColorMap[weather]
+          })
+        }
+      },
+      fail: () => {
+        // fail
+      },
+      complete: () => {
+        // complete
+        callback && callback()
+      }
+    })
+  }
 })
